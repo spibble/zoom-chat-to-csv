c652a311123dabe11afc03178260e4eb3e5f194f
cybedra <147957633+cybedra@users.noreply.github.com>
2025-07-28 19:00:43 -0700

    gained basic identity

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,31 @@
 console.log("Hello world")
+
+/*
+    Things to impl:
+    - in "Options", once a user uploads a file, add the name of the file after "File:" for clarity
+*/
+
+function toggleOptions(category: string) {
+    let checkbox = null;
+
+    switch(category) {
+        case "usernames":
+            checkbox = <HTMLInputElement>document.getElementById("extract-usernames");
+            break;
+        case "participation":
+            checkbox = <HTMLInputElement>document.getElementById("track-participation");
+            break;
+        default:
+            console.error(`Checkbox for category "${category}" not found.`);
+            return;
+    }
+
+    let optionsDivId = `${category}-options`;
+    let optionsDiv = <HTMLElement>document.getElementById(optionsDivId);
+
+    if (checkbox.checked == true) {
+        optionsDiv.style.display = "block";
+    } else {
+        optionsDiv.style.display = "none";
+    }
+}
